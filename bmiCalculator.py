e8fe326eb28aa8d92ee58f1241e976917c28a7b3
Sajith Prasad Bandara <25737034+SajithPrasadBandara@users.noreply.github.com>
2024-03-13 10:03:29 +0530

    Add files via upload

---
+++ bmiCalculator.py
@@ -0,0 +1,25 @@
+  
+def bmi_cal(mass,height):
+    height_in_meter = height/100
+
+    bmi = mass/(height_in_meter**2)
+    
+    if bmi > 0:
+        print("Your BMI : "+str(bmi))
+        if bmi < 18.5:
+            print("You are underweight.")
+        elif bmi < 24.9:
+            print("You are normal weight.")
+        elif bmi < 29.9:
+            print("You are overweight.")
+        else:
+            print("You are obese.")
+    else:
+        print("Enter valid inputs.")
+        
+        
+mass = int(input("Enter your weight(kg) : "))
+height = int(input("Enter your height(cm) : "))
+
+bmi_cal(mass,height)
+ 
